c00ac92a233e4b176eeda549fb96e7e3c04b30a0
Vega <aziaha.formations@gmail.com>
2025-12-03 14:32:34 +0100

    Découverte des variables de type scalaire en php

---
+++ 1_scalars_variables.php
@@ -0,0 +1,49 @@
+<?php
+
+    /*
+     *---------------------------------------------------------
+     * Les variables de type scalaire
+     * string, integer, float, boolean
+     *---------------------------------------------------------
+    */
+
+
+    //  boolean-------------------------------------------------------
+    $testCovid  = true;
+    $testGrippe = false;
+
+    // float-----------------------------------------------------------
+    $degree = 7.2;
+    $temperature = -5.5;
+
+
+    // integer---------------------------------------------------------
+    $code = 1234;
+    echo $code;
+
+    $chiffre = 1;
+
+    $temperature = -6;
+
+
+    // string-----------------------------------------------------------
+    // $bouteille = 'Eau';
+    // $bouteille = 'Soda';
+    // echo $bouteille;
+
+    // // Newsletter
+    // $email = "picsou@gmail.com";
+
+    //     // Si le format de l'email n'est pas valide
+    //     if ( ! filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+    //         // Alors, afficher le message d'erreur correspondant, 
+    //         // puis arrêter l'exécution du script. 
+    //         echo "Error";
+    //         exit();
+    //     }
+    //     // J'envoi un email à cette adresse.
+    //     // ...
+    // $phrase1 = 'Hello! C\'est Julie';
+    // $phrase2 = "Hello! C'est Julie";
+
+?>
