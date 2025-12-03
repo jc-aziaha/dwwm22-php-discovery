7a710cbc856a011aa12cb50d83b8cb8ff3df20cf
Vega <aziaha.formations@gmail.com>
2025-12-03 15:41:43 +0100

    Les variables composées

--- 2_composition_variables.php
+++ 2_composition_variables.php
@@ -1,2 +1,64 @@
 <?php
+
+    /*
+     *---------------------------------------------------------
+     * Les variables de type composé
+     * tableau, objet
+     *---------------------------------------------------------
+    */
+
+    // Tableau à double dimension
+    $players = [
+        0 => ['Kylian', 'Mbappé', 25],
+        1 => ['Ousmane', 'Dembélé', 28],
+        2 => ['Cristiano', 'Ronaldo', 40],
+        3 => ['Lionel', 'Messi', 37],
+    ];
+
+    echo $players[0][0] . " " . $players[0][1] . " a " . $players[0][2] . " ans.";
+
+    // echo $players[2][1];
+    // echo "<br>";
+    // echo $players[1][0];
+
+    // var_dump($players);
+
+    // <pre> est une astuce permettant d'indenter les informations affichée par le print_r
+    // echo "<pre>";
+    // print_r($players);
+    // echo "</pre>";
+
+
+    // tableau (réexpliqué)
+    // $personnages = ['Picsou', 'Donald', 'Daisy', 'Riri', 'Fifi', 'Loulou'];
+
+    // $personnages = [
+    //     0 => 'Picsou', 
+    //     1 => 'Donald', 
+    //     2 => 'Daisy', 
+    //     3 => 'Riri', 
+    //     4 => 'Fifi', 
+    //     5 => 'Loulou'
+    // ];
+
+    // // var_dump($personnages);
+    // echo $personnages[4];
+
+
+
+
+    // Tableau
+    // $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+    // $data = [1, 'Hello', true, 1.1];
+
+    // $fruits = [];
+    // // Afficher le tableau avant
+    // // echo $fruits;
+    // var_dump($fruits);
     
+    // $fruits[] = "pomme";
+    // // Afficher le tableau après
+    // var_dump($fruits);
+
+    // echo $fruits[0];
