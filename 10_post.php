b8463dadd4110b5b39d4a521ba6fc1e91c0038ac
Vega <aziaha.formations@gmail.com>
2025-12-09 14:34:16 +0100

    La variable super globale

---
+++ 10_post.php
@@ -0,0 +1,19 @@
+<?php
+    var_dump($_GET);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Inscription à la newsletter</h1>
+
+        <form method="get">
+            <input type="email" name="email">
+            <input type="submit">
+        </form>
+    </body>
+</html>
